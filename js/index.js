fda63e3ad42e352fd4ea83ee4a0b67072c656c0d
sonyavera <sonyavgould@gmail.com>
2020-08-08 19:36:08 -0400

    Solution 2

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const monsterContainer = document.getElementById("monster-container");
 const buttonDiv = document.getElementById("button-div");
 const nextButton = document.createElement('button');
 const previousButton = document.createElement('button');
+const createMonster = document.getElementById("create-monster")
 nextButton.innerText = `=>`
 previousButton.innerText = `<=`
 monsterContainer.nextElementSibling = buttonDiv;
@@ -11,11 +12,51 @@ buttonDiv.append(previousButton);
 buttonDiv.append(nextButton);
 let pageNumber = 0
 
+const createMonsterForm = () => {
+    const monsterForm = document.createElement('form')
+    monsterForm.innerHTML = 
+    `<label>Name: </label>
+    <input type="text" name="name">
+    <br>
+    <label>Age: </label>
+    <input type="text" name="age">
+    <br>
+    <label>Bio: </label>
+    <input type="text" name="bio">
+    <br>
+    <input type="submit" value="Create Monster">`
+    createMonster.append(monsterForm)
+}
+
+createMonster.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newMonsterForm = e.target
+    const name = newMonsterForm.name.value
+    const age = newMonsterForm.age.value
+    const bio = newMonsterForm.bio.value
+    const monsterObj = {
+        name: name,
+        bio: bio,
+        age: age,
+    }
+    renderMonster(monsterObj)
+    newMonsterForm.reset()
+
+    const options = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json"
+        },
+        body: JSON.stringify(monsterObj)
+      }
+
+      fetch('http://localhost:3000/monsters/', options) 
+    })
 
 
 buttonDiv.addEventListener("click", e => {    
     if(e.target === nextButton){
-            //document.querySelectorAll("div.monster").forEach(e => e.parentNode.removeChild(e)) //wat
             document.querySelectorAll("div.monster").forEach(e => e.remove())
             pageNumber = parseInt(pageNumber) + 1
         }else if(e.target === previousButton){
@@ -52,4 +93,5 @@ const renderMonster = (monster) => {
 
 
 getMonsters(0)
+createMonsterForm()
 });
