fe769b8f5e5509a36584e2b962161443e3cbe9d6
sonyavera <sonyavgould@gmail.com>
2020-08-08 18:50:08 -0400

    Solution 2

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-monsterData = {};
+let monsterData = {};
 const monsterContainer = document.getElementById("monster-container");
-const buttonDiv = document.createElement("div");
+const buttonDiv = document.getElementById("button-div");
 const nextButton = document.createElement('button');
 const previousButton = document.createElement('button');
 nextButton.innerText = `=>`
 previousButton.innerText = `<=`
+monsterContainer.nextElementSibling = buttonDiv;
 buttonDiv.append(previousButton);
 buttonDiv.append(nextButton);
-monsterContainer.append(buttonDiv);
+let pageNumber = 0
 
 
 
-buttonDiv.addEventListener("click", e => {
-        if(e.target === nextButton){
-            pageNumber += 1
+buttonDiv.addEventListener("click", e => {    
+    if(e.target === nextButton){
+            //document.querySelectorAll("div.monster").forEach(e => e.parentNode.removeChild(e)) //wat
+            document.querySelectorAll("div.monster").forEach(e => e.remove())
+            pageNumber = parseInt(pageNumber) + 1
         }else if(e.target === previousButton){
-            pageNumber -= 1
+            document.querySelectorAll("div.monster").forEach(e => e.remove())
+            pageNumber = parseInt(pageNumber) -  1
         }
         getMonsters(pageNumber)
     })
@@ -35,6 +39,7 @@ const getMonsters = (page) => {
 const renderMonster = (monster) => {
     const monsterLi = document.createElement("div")
     monsterLi.id = monster.id
+    monsterLi.classList.add("monster")
     monsterLi.innerHTML = 
     `${monster.name}<br>
     Age:${monster.age}<br>
